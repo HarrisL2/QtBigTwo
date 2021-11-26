e1236b83e489cc4e25099a43be62ddab253d0937
wcchanba <77112479+wcchanba@users.noreply.github.com>
2021-11-26 23:23:33 +0800

    Change combination.cpp

--- GraphicsScene/client/combination.cpp
+++ GraphicsScene/client/combination.cpp
@@ -1,4 +1,7 @@
+#include <QtDebug>
+
 #include "combination.h"
+
 #include "playingcard.h"
 #include "unocard.h"
 
@@ -26,7 +29,95 @@ Combination* Combination::createCombination(QVector<int> ids) {
         }
         if (cards.size() == 5) {
             //TODO 5 card checking
-            return new Combination(cards, Combination::Type::STRAIGHT_FLUSH);
+            QVector<BaseCard*> tempCards(cards);
+            for (int i = 0; i < 5; i++){
+                for (int j = i; j < 5; j++){
+                    if (tempCards[j]->getNumber() < tempCards[i]->getNumber()){
+                        BaseCard* temp = tempCards[j];
+                        tempCards[j] = tempCards[i];
+                        tempCards[i] = temp;
+                    }
+                }
+            }
+
+
+            if ((tempCards[0]->getNumber() + 1) == tempCards[1]->getNumber() && (tempCards[1]->getNumber() + 1) == tempCards[2]->getNumber() && (tempCards[2]->getNumber() + 1) == tempCards[3]->getNumber() && (tempCards[3]->getNumber() + 1) == tempCards[4]->getNumber()){
+                return new Combination(cards, Combination::Type::STRAIGHT);
+            }
+
+            if (tempCards[0]->getNumber() == 1 && tempCards[1]->getNumber() == 10 && (tempCards[1]->getNumber() + 1) == tempCards[2]->getNumber() && (tempCards[2]->getNumber() + 1) == tempCards[3]->getNumber() && (tempCards[3]->getNumber() + 1) == tempCards[4]->getNumber()){
+                return new Combination(cards, Combination::Type::STRAIGHT);
+            }
+
+            if (tempCards[0]->getNumber() == 1 && tempCards[1]->getNumber() == 2 && tempCards[2]->getNumber() == 11 && (tempCards[2]->getNumber() + 1) == tempCards[3]->getNumber() && (tempCards[3]->getNumber() + 1) == tempCards[4]->getNumber()){
+                return new Combination(cards, Combination::Type::STRAIGHT);
+            }
+
+            if (tempCards[0]->getType() == BaseCard::Type::PLAYING){
+                if (tempCards[0]->getSuit() == tempCards[1]->getSuit() && tempCards[0]->getSuit() == tempCards[2]->getSuit() && tempCards[0]->getSuit() == tempCards[3]->getSuit() && tempCards[0]->getSuit() == tempCards[4]->getSuit()){
+                    return new Combination(cards, Combination::Type::FLUSH);
+                }
+
+                if ((tempCards[0]->getNumber() + 1) == tempCards[1]->getNumber() && (tempCards[1]->getNumber() + 1) == tempCards[2]->getNumber() && (tempCards[2]->getNumber() + 1) == tempCards[3]->getNumber() && (tempCards[3]->getNumber() + 1) == tempCards[4]->getNumber()){
+                    if (tempCards[0]->getSuit() == tempCards[1]->getSuit() && tempCards[0]->getSuit() == tempCards[2]->getSuit() && tempCards[0]->getSuit() == tempCards[3]->getSuit() && tempCards[0]->getSuit() == tempCards[4]->getSuit()){
+                        return new Combination(cards, Combination::Type::STRAIGHT_FLUSH);
+                    }
+                }
+
+                if (tempCards[0]->getNumber() == 1 && tempCards[1]->getNumber() == 10 && (tempCards[1]->getNumber() + 1) == tempCards[2]->getNumber() && (tempCards[2]->getNumber() + 1) == tempCards[3]->getNumber() && (tempCards[3]->getNumber() + 1) == tempCards[4]->getNumber()){
+                    if (tempCards[0]->getSuit() == tempCards[1]->getSuit() && tempCards[0]->getSuit() == tempCards[2]->getSuit() && tempCards[0]->getSuit() == tempCards[3]->getSuit() && tempCards[0]->getSuit() == tempCards[4]->getSuit()){
+                        return new Combination(cards, Combination::Type::STRAIGHT_FLUSH);
+                    }
+                }
+
+                if (tempCards[0]->getNumber() == 1 && tempCards[1]->getNumber() == 2 && tempCards[2]->getNumber() == 11 && (tempCards[2]->getNumber() + 1) == tempCards[3]->getNumber() && (tempCards[3]->getNumber() + 1) == tempCards[4]->getNumber()){
+                    if (tempCards[0]->getSuit() == tempCards[1]->getSuit() && tempCards[0]->getSuit() == tempCards[2]->getSuit() && tempCards[0]->getSuit() == tempCards[3]->getSuit() && tempCards[0]->getSuit() == tempCards[4]->getSuit()){
+                        return new Combination(cards, Combination::Type::STRAIGHT_FLUSH);
+                    }
+                }
+            }
+
+            if (tempCards[0]->getType() == BaseCard::Type::UNO){
+                if (tempCards[0]->getColor() == tempCards[1]->getColor() && tempCards[0]->getColor() == tempCards[2]->getColor() && tempCards[0]->getColor() == tempCards[3]->getColor() && tempCards[0]->getColor() == tempCards[4]->getColor()){
+                    return new Combination(cards, Combination::Type::FLUSH);
+                }
+
+                if ((tempCards[0]->getNumber() + 1) == tempCards[1]->getNumber() && (tempCards[1]->getNumber() + 1) == tempCards[2]->getNumber() && (tempCards[2]->getNumber() + 1) == tempCards[3]->getNumber() && (tempCards[3]->getNumber() + 1) == tempCards[4]->getNumber()){
+                    if (tempCards[0]->getColor() == tempCards[1]->getColor() && tempCards[0]->getColor() == tempCards[2]->getColor() && tempCards[0]->getColor() == tempCards[3]->getColor() && tempCards[0]->getColor() == tempCards[4]->getColor()){
+                        return new Combination(cards, Combination::Type::STRAIGHT_FLUSH);
+                    }
+                }
+
+                if (tempCards[0]->getNumber() == 1 && tempCards[1]->getNumber() == 10 && (tempCards[1]->getNumber() + 1) == tempCards[2]->getNumber() && (tempCards[2]->getNumber() + 1) == tempCards[3]->getNumber() && (tempCards[3]->getNumber() + 1) == tempCards[4]->getNumber()){
+                    if (tempCards[0]->getColor() == tempCards[1]->getColor() && tempCards[0]->getColor() == tempCards[2]->getColor() && tempCards[0]->getColor() == tempCards[3]->getColor() && tempCards[0]->getColor() == tempCards[4]->getColor()){
+                        return new Combination(cards, Combination::Type::STRAIGHT_FLUSH);
+                    }
+                }
+
+                if (tempCards[0]->getNumber() == 1 && tempCards[1]->getNumber() == 2 && tempCards[2]->getNumber() == 11 && (tempCards[2]->getNumber() + 1) == tempCards[3]->getNumber() && (tempCards[3]->getNumber() + 1) == tempCards[4]->getNumber()){
+                    if (tempCards[0]->getColor() == tempCards[1]->getColor() && tempCards[0]->getColor() == tempCards[2]->getColor() && tempCards[0]->getColor() == tempCards[3]->getColor() && tempCards[0]->getColor() == tempCards[4]->getColor()){
+                        return new Combination(cards, Combination::Type::STRAIGHT_FLUSH);
+                    }
+                }
+            }
+
+            if (tempCards[0]->getNumber() == tempCards[1]->getNumber() && tempCards[0]->getNumber() == tempCards[2]->getNumber() && tempCards[3]->getNumber() == tempCards[4]->getNumber()){
+                return new Combination(cards, Combination::Type::FULL_HOUSE);
+            }
+
+            if (tempCards[0]->getNumber() == tempCards[1]->getNumber() && tempCards[2]->getNumber() == tempCards[3]->getNumber() && tempCards[2]->getNumber() == tempCards[4]->getNumber()){
+                return new Combination(cards, Combination::Type::FULL_HOUSE);
+            }
+
+            if (tempCards[0]->getNumber() == tempCards[1]->getNumber() && tempCards[0]->getNumber() == tempCards[2]->getNumber() && tempCards[0]->getNumber() == tempCards[3]->getNumber()){
+                return new Combination(cards, Combination::Type::FOUR_OF_A_KIND);
+            }
+
+            if (tempCards[1]->getNumber() == tempCards[2]->getNumber() && tempCards[1]->getNumber() == tempCards[3]->getNumber() && tempCards[1]->getNumber() == tempCards[4]->getNumber()){
+                return new Combination(cards, Combination::Type::FOUR_OF_A_KIND);
+            }
+
+            return nullptr;
         }
     }
     return nullptr;
@@ -49,10 +140,6 @@ BaseCard* Combination::getFirstCard() const {
 
 BaseCard* Combination::getLastCard() const {
     return cards.back();
-}
-
-Combination::Type Combination::getType() const {
-    return type;
 }
 
 int Combination::size() const {
